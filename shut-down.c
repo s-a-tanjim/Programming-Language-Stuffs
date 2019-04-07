c3b30e92ecab6f11c23f75b53242e506f04a157b
Shoeb Ahmed Tanjim <39959228+s-a-tanjim@users.noreply.github.com>
2019-04-07 21:49:50 +0600

    Add files via upload

---
+++ shut-down.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   system("C:\\WINDOWS\\System32\\shutdown /s");
+
+   //system("D:\\Music\\Godhulir_Opare_by_Shunno.mp3");
+
+   return 0;
+}
+
---
+++ threadTest.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<pthread.h>
+pthread_t t[2];
+int coin_flip;
+pthread_mutex_t flip_done;
+volatile int count,i;
+
+static void *thread2(void *_)
+{
+    printf("Count: %d ",count);
+    if(count==5) printf("Hello count %d\n",count);
+    for(i=0;i<10000000;i++);
+    printf("hhhhh");
+}
+
+static void *thread1(void *_)
+{
+    printf("Thread 1: flipped coin %d\n",coin_flip);
+    for(i=0;i<100;i++)
+    {
+        printf("%d ",i);
+        count++;
+        if(count==7) count=0;
+    }
+    printf("\n\n\n");
+    for(i=0;i<100;i++)
+    {
+        printf("%d ",i);
+        count++;
+        if(count==7) count=0;
+    }
+}
+
+
+void main()
+{
+    pthread_create(&t[1],NULL,thread2,NULL);
+    pthread_create(&t[0],NULL,thread1,NULL);
+
+
+    pthread_exit(NULL);
+}
